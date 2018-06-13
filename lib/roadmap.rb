73de16ec9554216a40337afcb7caad8142feb251
Thal Syhabout <tsyhabout@gmail.com>
2018-06-13 01:22:59 -0700

    add remaining checkoutpoint function

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -11,4 +11,9 @@ module Roadmap
        response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
        @checkpoint_data = JSON.parse(response.body)
   end
+
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get(api_url("enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"), headers: { "authorization" => @auth_token })
+    @remaining_data = JSON.parse(response.body)
+  end
 end
