2c04aad1ba3c91e276adbce0d8ba03e12b0ecbc3
Thal Syhabout <tsyhabout@gmail.com>
2018-06-13 00:46:58 -0700

    add message create and retrieve method

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,7 +16,6 @@ class Kele
   def get_me
      response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
      @user_data = JSON.parse(response.body)
-
   end
 
   def get_mentor_availability(mentor_id)
@@ -31,6 +30,27 @@ class Kele
     schedule_slot
 
   end
+
+  def get_messages(page_number = 1)
+    if page_number == nil
+      response = self.class.get(api_url("message_threads"), headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get(api_url("message_threads?page=#{page_number}"), headers: { "authorization" => @auth_token })
+    end
+      @messages = JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipent_id, token, subject, message)
+    response = self.class.post(api_url("messages"), headers: { "authorization" => @auth_token },
+    body: {
+        "sender" => sender,
+        "recipent_id" => recipent_id,
+        "token" => token,
+        "subject" => subject,
+        "stripeed-text" => message
+      })
+      puts response if response.success?
+    end
 
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
